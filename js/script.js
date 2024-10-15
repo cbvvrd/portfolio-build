377f4cd86966ca042304f930e5a2b69faeb12ea7
Charlie Bird <cbvvrd@gmail.com>
2024-10-15 16:54:15 +0100

    beginnings of fetch request & innerHTML manipulation

--- js/script.js
+++ js/script.js
@@ -1,27 +1,32 @@
 // Behaviour of buttons to open the modal boxes for portfolio projects //
+
 const infoButton1 = document.querySelector("#more-info-1");
 const infoButton2 = document.querySelector("#more-info-2");
 const infoButton3 = document.querySelector("#more-info-3");
 
-const portfolio1 = document.querySelector(".portfolio-1-large");
-const portfolio2 = document.querySelector(".portfolio-2-large");
-const portfolio3 = document.querySelector(".portfolio-3-large");
+const portfolio1Large = document.querySelector(".portfolio-1-large");
+const portfolio2Large = document.querySelector(".portfolio-2-large");
+const portfolio3Large = document.querySelector(".portfolio-3-large");
 
 infoButton1.addEventListener('click', () => {
-    portfolio1.classList.toggle('visible');
+    portfolio1Large.classList.toggle('visible');
 })
 
 infoButton2.addEventListener('click', () => {
-    portfolio2.classList.toggle('visible');
+    portfolio2Large.classList.toggle('visible');
 })
 
 infoButton3.addEventListener('click', () => {
-    portfolio3.classList.toggle('visible');
+    portfolio3Large.classList.toggle('visible');
 })
 
 // Getting portfolio data from JSON file //
 fetch('portfolio.json')
     .then(res => res.json())
-    .then(data =>
-        console.log(data)
-    );
+    .then(data => {
+        const portfolio1_img = document.querySelector("#project-1-img")
+        console.log(portfolio1_img)
+        portfolio1_img.innerHTML = `<img src="images/forest.jpg" alt="a webpage I have designed">`
+  
+
+        })
