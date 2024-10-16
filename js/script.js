e72ff4c6eaeb7c19181e9955947848a4135978f3
Charlie Bird <cbvvrd@gmail.com>
2024-10-16 11:19:17 +0100

    modal boxes now fetching data from JSON file

--- js/script.js
+++ js/script.js
@@ -1,36 +1,13 @@
-// Behaviour of buttons to open the modal boxes for portfolio projects //
-
-// const infoButton1 = document.querySelector("#more-info-1");
-// const infoButton2 = document.querySelector("#more-info-2");
-// const infoButton3 = document.querySelector("#more-info-3");
-
-// const portfolio1Large = document.querySelector(".portfolio-1-large");
-// const portfolio2Large = document.querySelector(".portfolio-2-large");
-// const portfolio3Large = document.querySelector(".portfolio-3-large");
-
-
-
-// infoButton1.addEventListener('click', () => {
-//     portfolio1Large.classList.toggle('visible');
-// })
-
-// infoButton2.addEventListener('click', () => {
-//     portfolio2Large.classList.toggle('visible');
-// })
-
-// infoButton3.addEventListener('click', () => {
-//     portfolio3Large.classList.toggle('visible');
-// })
-
-
 
 // Getting portfolio data from JSON file //
 fetch('portfolio.json')
     .then(res => res.json())
     .then(data => {
-
         const portfolioGallery = document.querySelector(".portfolio-grid");
+        const portfolioModals = document.querySelector(".portfolio-modals");
 
+
+        // insert portfolio gallery with data from JSON //
         data.projects.forEach(project => {
             
             portfolioGallery.innerHTML += `
@@ -44,15 +21,32 @@ fetch('portfolio.json')
                 </div> 
             `
 
+        // insert modal boxes with data from JSON //
 
-
+            portfolioModals.innerHTML += `
+                <div class="${project.modal_box}">
+                    <img src="${project.img}" alt="larger image of the webpage">
+                    <p>${project.info}</p>
+                    <a href="${project.site_link}" target="_blank">Live site</a>
+                    <a href="${project.github_link}" target="_blank"><i class="fa-brands fa-github"></i></a>
+                </div>      
+            `
         })
         
-   
+        // adding onClick function to "more info" buttons - open modals //
         
+        const portfolio1Large = document.querySelector(".portfolio-1-large");
+        const portfolio2Large = document.querySelector(".portfolio-2-large");
+        const portfolio3Large = document.querySelector(".portfolio-3-large");
 
-
-      
-  
+        document.querySelector("#more-info-1").addEventListener('click', () => {
+            portfolio1Large.classList.toggle('visible');
+        })
+        document.querySelector("#more-info-2").addEventListener('click', () => {
+            portfolio2Large.classList.toggle('visible');
+        })
+        document.querySelector("#more-info-3").addEventListener('click', () => {
+            portfolio3Large.classList.toggle('visible');
+        })
 
     })
